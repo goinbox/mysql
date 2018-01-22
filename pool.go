40b68b3b84864079970000c23cbb247c9bb84661
ligang <ligang1109@aliyun.com>
2018-01-22 10:36:01 +0800

    move from andals/gobox

---
+++ pool.go
@@ -0,0 +1,48 @@
+package mysql
+
+import (
+	"github.com/goinbox/pool"
+)
+
+type PConfig struct {
+	pool.Config
+
+	NewClientFunc func() (*Client, error)
+}
+
+type Pool struct {
+	p *pool.Pool
+
+	config *PConfig
+}
+
+type NewClientFunc func() (*Client, error)
+
+func NewPool(config *PConfig) *Pool {
+	this := &Pool{
+		config: config,
+	}
+
+	config.NewConnFunc = this.newConn
+
+	this.p = pool.NewPool(&this.config.Config)
+
+	return this
+}
+
+func (this *Pool) Get() (*Client, error) {
+	conn, err := this.p.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	return conn.(*Client), nil
+}
+
+func (this *Pool) Put(client *Client) error {
+	return this.p.Put(client)
+}
+
+func (this *Pool) newConn() (pool.IConn, error) {
+	return this.config.NewClientFunc()
+}
